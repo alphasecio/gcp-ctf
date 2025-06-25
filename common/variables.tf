4e03453a25191e9198c69aaafe1ec5c4b6e5c9de
alphasec <95212748+alphasecio@users.noreply.github.com>
2025-06-25 16:37:46 +0800

    Create variables.tf

---
+++ common/variables.tf
@@ -0,0 +1,19 @@
+variable "org_id" {
+  type = string
+}
+
+variable "billing_account_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "ctf_users_group" {
+  type = string
+}
